1b21fd83f4ee6cdaf3cf3cbb8d853db892d30e8a
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-07 10:05:19 -0700

    initialize files

---
+++ Programs/escape.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+  public class Rooms
+  {
+    private bool _bedroom;
+    private bool _kitchen;
+    private bool _livingroom;
+
+  }
+}
