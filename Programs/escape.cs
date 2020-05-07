c184aebcae2a04974eb8186512762ee55629bb23
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-07 10:17:40 -0700

    Build initial room constructors and enter methods

--- Programs/escape.cs
+++ Programs/escape.cs
@@ -5,9 +5,72 @@ namespace Game
 {
   public class Rooms
   {
-    private bool _bedroom;
-    private bool _kitchen;
-    private bool _livingroom;
+    public bool _bedroom { get; set; }
+    public bool _kitchen { get; set; }
+    public bool _livingroom { get; set; }
+    public bool _basement { get; set; }
+
+    public List<string> inventory { get; set; }
+    public string EnterBedroom()
+    {
+      if (_bedroom = true)
+      {
+        return "You are already in the bedroom.";
+      }
+      else
+      {
+        _bedroom = true;
+        _kitchen = false;
+        _livingroom = false;
+        _basement = false;
+        return "You enter the bedroom.";
+      }
+    }
+    public string EnterKitchen()
+    {
+      if (_kitchen = true)
+      {
+        return "You are already in the kitchen.";
+      }
+      else
+      {
+        _bedroom = false;
+        _kitchen = true;
+        _livingroom = false;
+        _basement = false;
+        return "You enter the kitchen.";
+      }
+    }
+    public string EnterLivingRoom()
+    {
+      if (_livingroom = true)
+      {
+        return "You are already in the livingroom.";
+      }
+      else
+      {
+        _bedroom = false;
+        _kitchen = false;
+        _livingroom = true;
+        _basement = false;
+        return "You enter the livingroom.";
+      }
+    }
+    public string EnterBasement()
+    {
+      if (_basement = true)
+      {
+        return "You are already in the basement.";
+      }
+      else
+      {
+        _bedroom = false;
+        _kitchen = false;
+        _livingroom = false;
+        _basement = true;
+        return "You cautiously enter the basement.";
+      }
+    }
 
   }
 }
